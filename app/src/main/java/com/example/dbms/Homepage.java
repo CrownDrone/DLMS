937af25f1168d2b290d709a131bf56f86f030fe6
CrownDrone <116888112+CrownDrone@users.noreply.github.com>
2023-04-21 15:04:58 +0800

    COMMITING DLMS

---
+++ app/src/main/java/com/example/dbms/Homepage.java
@@ -0,0 +1,34 @@
+package com.example.dbms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Homepage extends AppCompatActivity {
+    private Button logout;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        logout = (Button) findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+    private void logout(){
+        Intent main = new Intent(this, MainActivity.class);
+        startActivity(main);
+    }
+}
---
+++ app/src/main/java/com/example/dbms/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.dbms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login(){
+        Intent homepage = new Intent(this, Homepage.class);
+        startActivity(homepage);
+    }
+}
